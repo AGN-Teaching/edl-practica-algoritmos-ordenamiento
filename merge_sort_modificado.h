d1f984fb413e6094e548994441101b077ed44d3f
Abel García Nájera <abel.garcia.n@gmail.com>
2025-12-02 13:14:44 -0600

    Actualización de archivos de práctica de algoritmos de ordenamiento

--- merge_sort_modificado.h
+++ merge_sort_modificado.h
@@ -1,3 +1,7 @@
+#ifndef MERGESORT_MODIFICADO_H
+#define MERGESORT_MODIFICADO_H
+
+
 void merge(int A[], int inicio, int medio, int fin) {
     int *aux = new int[fin - inicio + 1];
     int i = inicio;     // índice de la primera mitad
@@ -43,51 +47,16 @@ void merge(int A[], int inicio, int medio, int fin) {
     delete [] aux;
 }
 
-void merge_sort(int A[], int inicio, int fin) {
-    if (inicio < fin) {
-        int medio = (inicio + fin) / 2;
-        merge_sort(A, inicio, medio);
-        merge_sort(A, medio + 1, fin);
-        merge(A, inicio, medio, fin);
-    }
+
+/**
+ * Merge sort modificado.
+ * @param A arreglo a ordenar.
+ * @param k tamaño del subarreglo para llamar a insertion sort directamente.
+ * @param inicio índice inicial del subarreglo a ordenar.
+ * @param fin índice final del subarreglo a ordenar.
+ */
+void merge_sort_modificado(int A[], int k, int inicio, int fin) {
+    // implementar
 }
 
-
-
-
-
-void intercambiar(int A[], int x, int y) {
-    int aux = A[x];
-    A[x] = A[y];
-    A[y] = aux;
-}
-
-int partition(int A[], int p, int r) {
-    // A[r] es el registro elegido cuya llave será el pivote
-    int pivote = A[r];
-    
-    // Indica el índice del último registro cuyo valor
-    // es menor a pivote
-    int ultimo = p - 1;
-
-    // Se agrupan los elementos menores a pivote en las primeras
-    // posiciones de A
-    for (int i = p; i < r; i++) {
-        if (A[i] < pivote) {
-            ultimo += 1;
-            intercambiar(A, ultimo, i);
-        }
-    }
-
-    // Y se mueve al pivote a su posición correcta
-    intercambiar(A, ultimo + 1, r);
-    return ultimo + 1;
-}
-
-void quicksort(int A[], int p, int r) {
-    if (p < r) {
-        int q = partition(A, p, r);
-        quicksort(A, p, q - 1);
-        quicksort(A, q + 1, r);
-    }
-}
+#endif // MERGESORT_MODIFICADO_H
